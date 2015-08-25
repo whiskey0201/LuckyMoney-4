22a762b5b3296c0da394ec2534015b4fbe079ea6
Kiyan Guass <fourcels@gmail.com>
2015-08-25 17:10:39 +0800

    Lucky Money

---
+++ app/src/main/java/com/fourcels/luckymoney/MainActivity.java
@@ -0,0 +1,72 @@
+package com.fourcels.luckymoney;
+
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.Button;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final Intent sSettingsIntent =
+            new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnAccessibility = (Button)findViewById(R.id.btn_accessibility);
+        btnAccessibility.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(sSettingsIntent);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private boolean isAccessibleEnabled() {
+        AccessibilityManager manager = (AccessibilityManager) getSystemService(Context.ACCESSIBILITY_SERVICE);
+
+        List<AccessibilityServiceInfo> runningServices = manager.getEnabledAccessibilityServiceList(AccessibilityEvent.TYPES_ALL_MASK);
+
+        for (AccessibilityServiceInfo info : runningServices) {
+            if (info.getId().equals(getPackageName() + "/.MonitorService")) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/fourcels/luckymoney/MonitorService.java
@@ -0,0 +1,136 @@
+package com.fourcels.luckymoney;
+
+import android.accessibilityservice.AccessibilityService;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MonitorService extends AccessibilityService {
+    private ArrayList<AccessibilityNodeInfo> mNodeInfoList = new ArrayList<AccessibilityNodeInfo>();
+
+    private boolean mLuckyMoneyOpened = true;
+
+    private final String HONGBAO_TEXT_KEY = "[微信红包]";
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        final int eventType = event.getEventType();
+
+        if (eventType == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {
+            System.out.println("TYPE_NOTIFICATION_STATE_CHANGED");
+            List<CharSequence> texts = event.getText();
+
+            if(!texts.isEmpty()){
+                for(CharSequence t: texts) {
+                    if(t.toString().contains(HONGBAO_TEXT_KEY)){
+                        openNotify(event);
+                        mLuckyMoneyOpened = false;
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        if(mLuckyMoneyOpened) {
+            System.out.println("there is no new lucky money.");
+            return;
+        }
+
+        if (eventType == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED) {
+            System.out.println("TYPE_WINDOW_CONTENT_CHANGED");
+            lingHongbao();
+        }
+
+
+        if (eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+            System.out.println("TYPE_WINDOW_STATE_CHANGED");
+            openHongbao(event);
+
+        }
+    }
+
+    private void openHongbao(AccessibilityEvent event){
+        String className = event.getClassName().toString();
+        if(className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI")){
+            chaiHongbao();
+            mLuckyMoneyOpened = true;
+        } else if(className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI")){
+            //goHome();
+            mLuckyMoneyOpened = true;
+        }
+    }
+
+
+    private void openNotify(AccessibilityEvent event){
+        System.out.println("openNotify");
+        if(event.getParcelableData() == null || !(event.getParcelableData() instanceof Notification)){
+            return;
+        }
+
+        Notification notification = (Notification) event.getParcelableData();
+        PendingIntent pendingIntent = notification.contentIntent;
+        try{
+            pendingIntent.send();
+        } catch (PendingIntent.CanceledException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void chaiHongbao(){
+        System.out.println("chaiHongbao");
+        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+        if(nodeInfo == null){
+            System.out.println("rootWindow为空");
+            return;
+        }
+        List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByText("Open");
+
+        for(AccessibilityNodeInfo n : list) {
+            n.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        }
+    }
+
+    private  void  lingHongbao(){
+        System.out.println("linHongbao");
+        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+        if(nodeInfo == null){
+            System.out.println("rootWindow为空");
+            return;
+        }
+
+        List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByText("领取红包");
+
+        if(!list.isEmpty()){
+            for(int i = list.size()-1; i>=0; i--) {
+                AccessibilityNodeInfo parent = list.get(i);
+                while (parent != null && !parent.isClickable()){
+                    parent = parent.getParent();
+                }
+                System.out.println("-->领取红包:" + parent);
+                if(parent != null) {
+                    parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                    break;
+                }
+            }
+        }
+
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, START_STICKY, startId);
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
